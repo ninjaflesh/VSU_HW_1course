631ef46dab3c7807ed8b5b4259ca060746001d4b
ninjaflesh <49476875+ninjaflesh@users.noreply.github.com>
2022-03-16 23:35:01 +0300

    Add files via upload

---
+++ HW_15_03/1.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void vvod(char* str);
+void vivod(char* str);
+int kolvo_chisel(char* str);
+
+int main() {
+	setlocale(LC_ALL, "rus");
+	const int size = 100;
+	char mass[size];
+
+	vvod(mass);
+	cout << kolvo_chisel(mass) << endl;
+
+	return 0;
+}
+
+void vvod(char* str)
+{
+	gets_s(str, 100);
+}
+void vivod(char* str)
+{
+	cout << str << endl;
+}
+int kolvo_chisel(char* str) {
+	int i = 0;
+	int chislo = 0;
+	while (i < strlen(str)) {
+		while ((str[i] == ' ') && (i < strlen(str))) {
+			i++;
+		}
+		while ((str[i] != ' ') && (i < strlen(str))) {
+			if ((str[i] == '0') || (str[i] == '1') ||
+				(str[i] == '2') || (str[i] == '3') ||
+				(str[i] == '4') || (str[i] == '5') ||
+				(str[i] == '6') || (str[i] == '7') ||
+				(str[i] == '8') || (str[i] == '9')) {
+				chislo++;
+			}
+			i++;
+		}
+	}
+	return chislo;
+}
---
+++ HW_15_03/2.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void vvod(char* str);
+void vivod(char* str);
+void palendrom(char* str);
+
+int main() {
+	setlocale(LC_ALL, "rus");
+	const int size = 100;
+	char mass[size];
+
+	vvod(mass);
+	palendrom(mass);
+
+	return 0;
+}
+
+void vvod(char* str)
+{
+	gets_s(str, 100);
+}
+
+void vivod(char* str)
+{
+	cout << str << endl;
+}
+
+void palendrom(char* str) {
+	int j;
+	char str2[100];
+	for (int i = 0; i < strlen(str); i++) {
+		while ((str[i] == ' ') && (i < strlen(str))) {
+			i++;
+		}
+
+		j = 0;
+
+		while ((str[i] != ' ') && (i < strlen(str))) {
+			str2[j++] = str[i++];
+		}
+		str2[j] = '\0';
+
+		bool flag = true;
+		for (int k = 0; k < strlen(str2) / 2; k++) {
+			if (str2[k] != str2[strlen(str2) - k - 1]) {
+				flag = false;
+			}
+		}
+
+		if (!flag) {
+			cout << "Не полиндром ";
+			vivod(str2);
+		}
+		else {
+			cout << "Полиндром ";
+			vivod(str2);
+		}
+	}
+}
